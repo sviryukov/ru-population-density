98788e19becd4986a2c424462f115990b5666002
sviryukov <lirik1198KZ>
2019-12-08 21:42:58 +0300

    Create map component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {Map} from "./components/Map";
+
+const App = () => {
+    return (
+        <div style={{width: '1280px', height: '720px', margin: 'auto'}}>
+            <Map/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,29 @@
+import {ComposableMap, Geographies, Geography} from 'react-simple-maps';
+import {geoConicEqualArea} from "d3-geo";
+import topo from '../geography/topo';
+
+const Map = () => {
+    return (
+        <ComposableMap projection={() => geoConicEqualArea()
+            .scale(1000)
+            .center([-32, 72.5])
+            .parallels([40, 80])
+            .rotate([270, 0])} width='1280' height='720'>
+            <Geographies geography={topo}>
+                {(geographies, projection) => geographies.map(geo => {
+                    return (
+                        <Geography
+                            key={geo.properties.full_name}
+                            stroke='#fff'
+                            projection={projection}
+                            geography={geo}
+                            fill={geo.properties.fill}
+                        />
+                    );
+                })}
+            </Geographies>
+        </ComposableMap>
+    );
+};
+
+export {Map};
