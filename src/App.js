28cf9a390f40aac7baeb6a11ce5219ef886eea0b
sviryukov <lirik1198KZ>
2019-12-09 12:21:19 +0300

    Create header and legend components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import {Map} from "./components/Map";
+import {useState} from 'react';
+import {Header} from "./components/Header";
+import {Legend} from './components/Legend';
+import {Map} from './components/Map';
+
+const COLOR_HUE = 270;
 
 const App = () => {
+    const [selectedSubject, setSelectedSubject] = useState(false);
+    const [breakpoints, setBreakpoints] = useState(false);
     return (
-        <div style={{width: '1280px', height: '720px', margin: 'auto'}}>
-            <Map/>
+        <div style={{width: '1280px', height: '900px', margin: 'auto', padding: '20px 0px', fontFamily: 'sans-serif'}}>
+            <Header/>
+            <Legend breakpoints={breakpoints}
+                    selectedSubject={selectedSubject}
+                    setSelectedSubject={setSelectedSubject}
+                    colorHue={COLOR_HUE}/>
+            <Map setBreakpoints={setBreakpoints}
+                 selectedSubject={selectedSubject}
+                 setSelectedSubject={setSelectedSubject}
+                 colorHue={COLOR_HUE}/>
         </div>
     );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div style={{width: '100%', textAlign: 'center'}}>
+            <h1>
+                Плотность населения субъектов Российской Федерации
+            </h1>
+        </div>
+    );
+};
+
+export {Header};
